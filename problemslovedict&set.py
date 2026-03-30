35f78b4fd704615bc18fefc81f4ca7afd50b584a
naimul1322 <naimulislam1322@gamil.com>
2026-03-30 23:49:06 +0600

    dict & set

---
+++ problemslovedict&set.py
@@ -0,0 +1,18 @@
+# problem sloving 
+
+room ={"python","java","c++","python","java","c++","c","python","javascript"}
+
+print(len(room))
+
+
+# marks={}
+
+# x=int(input("enter phy: "))
+# marks.update({"phy": x})
+
+# x=int(input("enter math: "))
+# marks.update({"math": x})
+# x=int(input("enter che: "))
+# marks.update({"che": x})
+
+# print(marks)
---
+++ set.py
@@ -0,0 +1,7 @@
+# set in python programming
+# set in a unique and sorted data asending order
+
+nums={2,2,2,4,5,6,8,2,5,6,7,1}
+print(nums)
+print(len(nums))
+print(type(nums))
