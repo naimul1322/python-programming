d695f5c94063b769471c5a9a3207908963cfd7b0
naimul1322 <naimulislam1322@gamil.com>
2026-03-31 12:57:37 +0600

    sum & factorial problem

---
+++ sumofNnumber.py
@@ -0,0 +1,17 @@
+# sum of First n numbers
+
+# n=int(input("Enter the number: "))
+# idx=1
+# sum=0
+# while idx<=n:
+#     sum+=idx
+#     idx+=1
+# print(sum)
+
+# factorial of first n numbers
+
+n=int(input("Enter your number: "))
+sum=1
+for i in range(1,n+1):
+    sum*=i
+print(sum)
